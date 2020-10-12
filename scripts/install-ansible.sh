a6d4cefdeb068757adf0bf95c8ffcec2fc42f608
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-10-12 08:31:04 +0530

    update

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -eux
 
-yum -y 
+# Install Ansible repository.
+# amazon linux
+# sudo yum -y install python3
+# sudo pip3 install ansible
+
+# #apt
+# sudo apt -y update
+# sudo apt -y install software-properties-common
+# sudo apt-add-repository ppa:ansible/ansible
+
+# # Install Ansible.
+# sudo apt -y update
+# sudo apt -y install ansible
